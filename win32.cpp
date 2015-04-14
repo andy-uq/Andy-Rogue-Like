de05f0d4305d6761e29cac724aa2275dcc748c88
Andy Clarke <andy.uq@gmail.com>
2015-04-14 13:36:34 +1200

    Step 3 complete

--- win32.cpp
+++ win32.cpp
@@ -2,7 +2,41 @@
 #include <stdio.h>
 #include <tchar.h>
 
-static HANDLE hStdout, hActiveBuffer, hBackBuffer;
+static HANDLE hStdin, hStdout, hActiveBuffer, hBackBuffer;
+static int fdwSaveOldMode;
+static bool alive;
+
+short x = 9, y = 9;
+
+void debug(wchar_t* outputString)
+{
+	OutputDebugString(outputString);
+}
+
+void debug(char* outputString)
+{
+	OutputDebugStringA(outputString);
+}
+
+void debugf(const wchar_t* format, ...)
+{
+	va_list argp; 
+	va_start(argp, format);
+	wchar_t buffer[512];
+	vswprintf_s(buffer, 512, format, argp);
+	OutputDebugString(buffer);
+	va_end(argp);
+}
+
+void debugf(const char* format, ...)
+{
+	char buffer[512];
+	va_list argp;
+	va_start(argp, format);
+	vsprintf_s(buffer, 512, format, argp);
+	OutputDebugStringA(buffer);
+	va_end(argp);
+}
 
 int win32_init()
 {
@@ -20,27 +54,122 @@ int win32_init()
 
 	hActiveBuffer = CreateConsoleScreenBuffer(
 		GENERIC_READ | GENERIC_WRITE,				// read/write access
-		FILE_SHARE_READ | FILE_SHARE_WRITE,			// shared 
-		NULL,										// default security attributes 
-		CONSOLE_TEXTMODE_BUFFER,					// must be TEXTMODE 
-		NULL										// reserved; must be NULL 
+		FILE_SHARE_READ | FILE_SHARE_WRITE, 		// shared 
+		NULL, 										// default security attributes 
+		CONSOLE_TEXTMODE_BUFFER, 					// must be TEXTMODE 
+		NULL 										// reserved; must be NULL 
 		);
 
 	if (hStdout == INVALID_HANDLE_VALUE || hActiveBuffer == INVALID_HANDLE_VALUE || hBackBuffer == INVALID_HANDLE_VALUE)
 	{
-		printf("CreateConsoleScreenBuffer failed - (%d)\n", GetLastError());
+		debugf("CreateConsoleScreenBuffer failed - (%d)\n", GetLastError());
 		return 1;
 	}
 
 	// Make the new screen buffer the active screen buffer. 
 
-	if (SetConsoleActiveScreenBuffer(hActiveBuffer))
+	if (!SetConsoleActiveScreenBuffer(hActiveBuffer))
 	{
-		return 0;
+		debugf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
+		return 1;
 	}
 
-	printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
-	return 1;
+	hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	if (hStdin == INVALID_HANDLE_VALUE)
+	{
+		debugf("GetStdHandle failed - (%d)\n", GetLastError());
+		return 1;
+	}
+
+	int fdwMode = 0;
+	if (!SetConsoleMode(hStdin, fdwMode))
+	{
+		debugf("SetConsoleMode failed - (%d)\n", GetLastError());
+		return 1;
+	}
+
+	CONSOLE_CURSOR_INFO cursorInfo = {};
+	cursorInfo.bVisible = false;
+	cursorInfo.dwSize = 100;
+
+	SetConsoleCursorInfo(hActiveBuffer, &cursorInfo);
+	SetConsoleCursorInfo(hBackBuffer, &cursorInfo);
+	return 0;
+}
+
+VOID KeyEventProc(KEY_EVENT_RECORD ker)
+{
+	debugf("Key event: ");
+
+	if (ker.bKeyDown)
+	{
+		debugf("key pressed '%c'\n", ker.uChar.AsciiChar);
+	}
+	else 
+	{
+		switch (ker.wVirtualKeyCode)
+		{
+		case 0x51:
+			alive = false;
+			break;
+
+		case VK_DOWN:
+			y++;
+			break;
+
+		case VK_UP:
+			y--;
+			break;
+
+		case VK_LEFT:
+			x--;
+			break;
+
+		case VK_RIGHT:
+			x++;
+			break;
+
+		default:
+			debugf("key released '%c'\n", ker.uChar.AsciiChar);
+		}
+	}
+}
+
+int processInput()
+{
+	DWORD cNumRead, i;
+	INPUT_RECORD irInBuf[128];
+
+	if (!ReadConsoleInput(
+		hStdin,      // input buffer handle 
+		irInBuf,     // buffer to read into 
+		128,         // size of read buffer 
+		&cNumRead))  // number of records read 
+	{
+		return 1;
+	}
+
+	for (i = 0; i < cNumRead; i++)
+	{
+		switch (irInBuf[i].EventType)
+		{
+		case KEY_EVENT: // keyboard input 
+			KeyEventProc(irInBuf[i].Event.KeyEvent);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	return 0;
+}
+
+int beginRender()
+{
+	DWORD write;
+	FillConsoleOutputCharacter(hBackBuffer, _T(' '), 80 * 25, {}, &write);
+	return 0;
 }
 
 int renderComplete()
@@ -56,6 +185,7 @@ int renderComplete()
 
 int win32_close()
 {
+	SetConsoleMode(hStdin, fdwSaveOldMode);	
 	return SetConsoleActiveScreenBuffer(hStdout);
 }
 
@@ -73,9 +203,9 @@ int drawToBuffer(char *text)
 		chiBuffer[i].Attributes = 0xf7;
 	}
 
-	srctWriteRect.Top = 10;    // top lt: row 10, col 0 
+	srctWriteRect.Top = 0;    // top lt: row 10, col 0 
 	srctWriteRect.Left = 0;
-	srctWriteRect.Bottom = 11; // bot. rt: row 11, col 79 
+	srctWriteRect.Bottom = 2; // bot. rt: row 11, col 79 
 	srctWriteRect.Right = 80;
 
 	// Copy from the temporary buffer to the new screen buffer. 
@@ -89,8 +219,6 @@ int drawToBuffer(char *text)
 	if (fSuccess)
 	{
 		renderComplete();
-
-		Sleep(1000);
 		return 0;
 	}
 
@@ -98,12 +226,69 @@ int drawToBuffer(char *text)
 	return 1;
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+int drawCharAt(short x, short y, char c)
+{
+	CHAR_INFO chiBuffer = {};
+	chiBuffer.Attributes = 0x07;
+	chiBuffer.Char.AsciiChar = c;
+
+	SMALL_RECT writeRect;
+	writeRect.Left = x;
+	writeRect.Top = y;
+	writeRect.Bottom = y + 1;
+	writeRect.Right = x + 1;
+
+	WriteConsoleOutput(
+		hBackBuffer,
+		&chiBuffer,
+		{ 1, 1 },
+		{},
+		&writeRect);
+
+	return 0;
+}
+
+int _tmain()
 {
 	if (win32_init())
 		return 1;
 
-	drawToBuffer("Hello world");
+	alive = true;
+	while (alive)
+	{
+		beginRender();
+
+		bool bumpIntoWall = false;
+		if (x < 0)
+		{
+			x = 0;
+			bumpIntoWall = true;
+		}
+		if (y < 0)
+		{
+			y = 0;
+			bumpIntoWall = true;
+		}
+		if (y > 19)
+		{
+			y = 19;
+			bumpIntoWall = true;
+		}
+		if (x > 19)
+		{
+			x = 19;
+			bumpIntoWall = true;
+		}
+		if (bumpIntoWall)
+		{
+			drawToBuffer("You bumped into a wall");
+		}
+
+		drawCharAt(x + 30, y + 2, '@');
+		renderComplete();
+
+		processInput();
+	}
 
 	win32_close();
 	return 0;
